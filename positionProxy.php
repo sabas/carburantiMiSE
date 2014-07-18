649b0b09d1445662711574af25b8e482af280d81
sabas <sabas88@gmail.com>
2014-07-18 17:11:44 +0200

    Initial commit

---
+++ positionProxy.php
@@ -0,0 +1,21 @@
+<?php
+
+//open connection
+$ch = curl_init();
+
+//set the url, number of POST vars, POST data
+
+curl_setopt($ch,CURLOPT_URL, "https://carburanti.mise.gov.it/OssPrezziSearch/ricerca/position");
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, true);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, "pointsListStr=".$_GET['pointsListStr']);
+
+$request= curl_exec($ch);
+
+//close connection
+curl_close($ch);
+echo $request;
+
+?>
