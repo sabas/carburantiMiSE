fc64b30c9cfffc537f9c1f72d7d4a4baf3aac882
sabas <sabas88@gmail.com>
2014-07-25 11:28:54 +0200

    scrape.py ottimizzato - marco

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -8,8 +8,10 @@ import re
 with open(sys.argv[1]) as file:
 	data=json.load(file)
 	data=data['array']
-	db = sqlite3.connect('db/scrape.sqlite')
-	cursor = db.cursor()
+	dbcon = sqlite3.connect('db/scrape.sqlite')
+	dbcon.isolation_level = None
+	cursor = dbcon.cursor()
+	cursor.execute('begin')
 	regex = re.compile(".*#(.*)\((.*)\)")
 	for d in data:
 		addr=d['addr']
@@ -21,10 +23,14 @@ with open(sys.argv[1]) as file:
 		cursor.execute('''INSERT OR REPLACE INTO `distributori`(`id`, `name`, `bnd`, `lat`, `lon`, `addr`, `comune`, `provincia`) 
                   VALUES (:id,:name,:bnd,:lat,:lon,:addr,:comune,:provincia)''',
                   {'id':d['id'],'name':d['name'],'bnd':d['bnd'],'lat':d['lat'],'lon':d['lon'],'addr':d['addr'],'comune':grp[0].title(),'provincia':grp[1]})
+                
                 carb=d['carburanti']
+                
                 for c in carb:
  			cursor.execute('''INSERT OR REPLACE INTO `prezzi`(`id_d`, `dIns`, `carb`, `isSelf`, `prezzo`)  
                 	  VALUES (:id_d,:dIns,:carb,:isSelf,:prezzo)''',
                 	  {'id_d':d['id'],'dIns':d['dIns'],'carb':c['carb'],'isSelf':c['isSelf'],'prezzo':c['prezzo']})               	
-		db.commit()
-	db.close()
+
+	cursor.execute('end')
+	dbcon.commit()
+	dbcon.close()
